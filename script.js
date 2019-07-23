e3578677e7addea82bbe8101eef7fe944af6ffe0
shusiner <quantum.plato@gmail.com>
2019-07-24 00:21:41 +0800

    modified to download capture

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 var fileChooser = document.getElementsByTagName('input')[0];
 var content = document.getElementById('content');
+var download = document.getElementById('download');
 
 if (typeof window.FileReader === 'undefined') {
     content.className = 'fail';
     content.innerHTML = 'File API &amp; FileReader API are not supported in your browser.  Try on a new-ish Android phone.';
+}
+
+function downloadFile(data, fileName, type="text/plain") {
+    // Create an invisible A element
+    const a = document.createElement("a");
+    a.style.display = "none";
+    document.body.appendChild(a);
+  
+    // Set the HREF to a Blob representation of the data to be downloaded
+    a.href = window.URL.createObjectURL(
+      new Blob([data], { type })
+    );
+  
+    // Use download attribute to set set desired file name
+    a.setAttribute("download", fileName);
+  
+    // Trigger the download by simulating click
+    a.click();
+  
+    // Cleanup
+    window.URL.revokeObjectURL(a.href);
+    document.body.removeChild(a);
+  }
+
+download.onclick = function (e) {
+    if (!fileChooser.files[0]) {
+        alert("please take a photo or attach a file first")
+    } else {
+    console.log(fileChooser.files[0]["type"]);
+    console.log(fileChooser.files[0]["name"]);
+    downloadFile(fileChooser.files[0], 'capture.jpg', fileChooser.files[0]["type"])
+    }
+
 }
 
 fileChooser.onchange = function (e) {
